1e2b760192eeeb7dad29110c7b37fc910e1a6610
Pieter Berteloot <Pieter Berteloot>
2016-03-23 20:30:46 +0100

    Added documentation

--- Excercise 2 - Cardgame/ArrayHold.java
+++ Excercise 2 - Cardgame/ArrayHold.java
@@ -2,15 +2,26 @@ package Exercise2;
 
 public class ArrayHold {
 
+	// the hold array
 	static int[] hold = new int[52]; 
 	
+	/**
+	 * Adds 2 equal cards to the hold deck
+	 * @param a the number of the cards
+	 */
 	public static void addCardsToHold(int a){
 		
 		hold[getLastElement(hold)] = a;
 		hold[getLastElement(hold)] = a;
 		
 	}
+
 	
+	/**
+	 * Returns the index of the first element that is equal to 0 in an array
+	 * @param ar the array
+	 * @return	the index of the first 0 in the array
+	 */
 	public static int getLastElement(int[] ar){
 		
 		int b = 0;
@@ -24,7 +35,10 @@ public class ArrayHold {
 	}
 	
 	
-	
+	/**
+	 * adds all the hold cards to the winning deck
+	 * @param ar	the winning deck
+	 */
 	public static void addHoldToDeck(int[] ar){
 		
 		while (hold[0] != 0){
