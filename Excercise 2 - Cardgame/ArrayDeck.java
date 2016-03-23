3d518e3d021f09fbd68ed6da4cd37fde13941061
Pieter Berteloot <Pieter Berteloot>
2016-03-23 20:40:14 +0100

    Added documentation

--- Excercise 2 - Cardgame/ArrayDeck.java
+++ Excercise 2 - Cardgame/ArrayDeck.java
@@ -1,17 +1,18 @@
 package Exercise2;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ArrayDeck {
-	
+
 	static int[] deckPlayer1 = new int[54];
 	static int[] deckPlayer2 = new int[54];
-	
-
 
 	
+	/**
+	 * shuffles an array
+	 * @param ar the array you want to shuffle
+	 */
 	static void shuffleDeck(int[] ar) {
 
 		Random rnd = ThreadLocalRandom.current();
@@ -22,22 +23,21 @@ public class ArrayDeck {
 			ar[i] = a;
 		}
 	}
-	
+
+	/**
+	 * Splits the deck into 2 parts
+	 * @param ar The array you want to split
+	 */
 	public static void splitDeck(int[] ar) {
-		
-		for (int j = 0; j < ar.length/2; j++) {
+
+		for (int j = 0; j < ar.length / 2; j++) {
 			deckPlayer1[j] = ar[j];
 		}
-		
-		for (int j = 26; j < ar.length; j++) {
-			deckPlayer2[j-26] = ar[j];
-		}
-	
 
+		for (int j = 26; j < ar.length; j++) {
+			deckPlayer2[j - 26] = ar[j];
+		}
 
 	}
-	
-	
-	
 
 }
