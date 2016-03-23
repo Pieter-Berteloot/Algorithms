2286a4b6e501453a91a8fe4a6ed9b3d57abf7ab5
Pieter Berteloot <Pieter Berteloot>
2016-03-23 20:37:16 +0100

    Added documentation

--- Excercise 2 - Cardgame/AddandRemove.java
+++ Excercise 2 - Cardgame/AddandRemove.java
@@ -1,29 +1,36 @@
 package Exercise2;
 
-
-
 public class AddandRemove {
 	static int indexToAdd;
 
-	
-	public static void add(int[] winner, int[] loser){
-		
+	/**
+	 * Adds both cards to the winning deck
+	 * @param winner 	The winning deck
+	 * @param loser 	The losing deck
+	 */
+	public static void add(int[] winner, int[] loser) {
+
 		indexToAdd = ArrayHold.getLastElement(winner);
-		
-		winner[indexToAdd] = winner[0] ;
-		winner[indexToAdd+1] = loser[0];
-	
-		
+
+		winner[indexToAdd] = winner[0];
+		winner[indexToAdd + 1] = loser[0];
+
 	}
-	public static void remove(int[] winner, int[] loser){
-		
-		for(int i=0; i<winner.length-1; i++){ 
-		     winner[i] = winner[i+1];
+
+	/**
+	 * Removes the cards from the winning and losing deck
+	 * @param winner 	The winning deck
+	 * @param loser 	The losing deck
+	 */
+	public static void remove(int[] winner, int[] loser) {
+
+		for (int i = 0; i < winner.length - 1; i++) {
+			winner[i] = winner[i + 1];
 		}
-		
-		for(int i=0; i<loser.length-1; i++){ 
-		     loser[i] = loser[i+1];
+
+		for (int i = 0; i < loser.length - 1; i++) {
+			loser[i] = loser[i + 1];
 		}
-		
+
 	}
 }
