7f3ff1f6652cd5c28eab9a32ecb8b659655a72e5
Pieter Berteloot <Pieter Berteloot>
2016-04-20 00:45:25 +0200

    Changed Arraylist children to type node, added node parent and deleted depth.

--- Excercise 3 - Tree without HashMap/Node.java
+++ Excercise 3 - Tree without HashMap/Node.java
@@ -4,32 +4,29 @@ import java.util.ArrayList;
 
 public class Node {
 
-
 	private String identifier;
-	private int depth;
-	private ArrayList<String> children;
+	private ArrayList<Node> children;
+	private Node parent;
 
-	public Node(String a, int depth) {
+	public Node(String a, Node parent) {
 		identifier = a;
-		children = new ArrayList<String>();
-		this.depth = depth;
+		children = new ArrayList<Node>();
+		this.parent = parent;
 	}
 
 	public String getIdentifier() {
 		return identifier;
 	}
 
-	public ArrayList<String> getChildren() {
+	public ArrayList<Node> getChildren() {
 		return children;
-
 	}
 
-	public void addChild(String identifier) {
-		children.add(identifier);
+	public void addChild(Node child) {
+		children.add(child);
 	}
 
-	public int getDepth(){
-		
-		return depth;
+	public Node getParent() {
+		return parent;
 	}
 }
