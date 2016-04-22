1ba043b01854c23565bc22a5e903b40ec319881d
Pieter Berteloot <Pieter Berteloot>
2016-04-22 21:46:21 +0200

    Edited user input, added amound of elements in combination

--- Excercise 3 - Tree without HashMap/UserInput.java
+++ Excercise 3 - Tree without HashMap/UserInput.java
@@ -6,31 +6,35 @@ public class UserInput {
 
 	public ArrayList<String> elements = new ArrayList<String>();
 
-
 	public static void main(String[] args) {
 
-
 		Tree tree = new Tree();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Type in the Tree elements.");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Type in the Tree elements. Type finish when ready");
 
 		/**
-		 * Allows the user to input elements
-		 * when finished, the combinations and permutations
-		 * will be printed.
+		 * Allows the user to input elements when finished, the combinations and
+		 * permutations will be printed.
 		 */
-		
-		while (true) {
-			String next = scanner.next();
 
-			if ((next.equals("finish") || next.equals("Finish"))) {
+		while (true) {
+
+			String stringInput = input.nextLine();
+			if ((stringInput.equals("finish") || stringInput.equals("Finish"))) {
 				tree.makeTree(tree.getElements(), tree.getRoot());
 
-				Combinations.printAllCombitnations(tree.getRoot());
 				Permutations.printAllPermutations(tree.getLastNodes(), tree.getRoot());
+				System.out.println("Type combinations to calculate the combinations");
+			
+				System.out.println("Enter the amound of elements you want to calculate the Combinations.");
+				while (input.hasNextInt()) {
+					Combinations.printAllCombinations(tree.getLastNodes(), tree.getRoot(), input.nextInt(),
+							tree.getElements().size());
+				
+			}} else {
 
-			} else {
-				tree.addElements(next);
+				tree.addElements(stringInput);
+
 			}
 		}
 	}
