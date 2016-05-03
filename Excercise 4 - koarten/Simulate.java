7c09f8e5aab2d128de09b06379f79c8b18d2eeb1
Pieter Berteloot <Pieter Berteloot>
2016-05-03 20:17:36 +0200

    Made code shorter, deleted boolean

--- Excercise 4 - koarten/Simulate.java
+++ Excercise 4 - koarten/Simulate.java
@@ -1,18 +1,15 @@
 import org.graphstream.graph.Edge;
 
-
 public class Simulate {
 
 	private Map map;
-
 	public Simulate(Map map) {
-		this.map = map;	
+		this.map = map;
 	}
 
 	private boolean simulateAccident(Edge e) {
 
 		int carAmount = (int) e.getNumber("carAmount");
-
 		if (Math.random() < (0.001 * carAmount)) {
 			e.addAttribute("ui.class", "important");
 			return true;
@@ -22,18 +19,15 @@ public class Simulate {
 
 	private void addWeightonAccident(Edge e) {
 
-		boolean accident = false;
 		if (simulateAccident(e) == true) {
-
 			e.changeAttribute("weight", e.getNumber("weight") + 30);
 			e.addAttribute("label", "" + (int) e.getNumber("weight"));
-			System.out.println("Accident on: " + e.getId() + " +30");
-			accident = simulateAccident(e);
-			while (accident == true) {
+			System.out.println("Accident on: " + e.getId() + " +30 weight");
+			
+			while (simulateAccident(e) == true) {
 				e.changeAttribute("weight", e.getNumber("weight") + 50);
 				e.addAttribute("label", "" + (int) e.getNumber("weight"));
-				System.out.println("Accident on: " + e.getId() + " +50");
-				accident = simulateAccident(e);
+				System.out.println("Accident on: " + e.getId() + " +50 weight");
 			}
 		}
 	}
